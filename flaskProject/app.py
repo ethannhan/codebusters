9a881c83531b10678b821e5c3dbe727ad35bfa77
SAMUEL <samfergot@gmail.com>
2021-11-10 18:27:03 -0500

    Update app.py

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<a href="/login" >Login</a>'
+
+@app.route('/login')
+def login():
+    return """<form action="/action_page.php">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>"""
 
 
 if __name__ == '__main__':
