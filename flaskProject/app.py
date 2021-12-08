b963bd353280fb615c7e4944bc94ca7baceeac94
Nitya Cohen <nityacohen10@gmail.com>
2021-12-07 20:31:36 -0500

    image now displays on website.

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, current_app, request, redirect, url_for, flash
+from flask import Flask, render_template, send_from_directory, current_app, request, redirect, url_for, flash
 from flask_socketio import SocketIO
 from werkzeug.utils import secure_filename
 import os
 
-UPLOAD_PATH = "./profile_pictures"
+UPLOAD_FOLDER = "./profile_pictures"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.config['UPLOAD_PATH'] = UPLOAD_PATH
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 socketio = SocketIO(app)
 
 
@@ -43,9 +43,19 @@ def profile_picture():
     filename = secure_filename(uploaded_file.filename)
     if filename:
         print("filename is: {}".format(uploaded_file.filename))
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return redirect("http://localhost:5000")
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for("uploaded_file", filename=filename))
 
+
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    filename = 'http://127.0.0.1:5000/uploads/' + filename
+    return render_template('index.html', filename=filename)
+
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 
 if __name__ == '__main__':
