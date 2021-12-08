df402d90955c4be3e49200c16a2b5ec8723caaf4
Ethan Nhan <ethannha@buffalo.edu>
2021-12-07 20:38:07 -0500

    basic registration and login functionality works

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, current_app, request, make_response
 from flask_socketio import SocketIO
 import helper_functions
 import pymongo
+import sys
 import bcrypt
 
 myclient = pymongo.MongoClient('mongo', 27017)
 userdatabase = myclient["accounts"]
-userCollection = userdatabase.users
+userCollection = userdatabase['users']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -28,10 +29,11 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if userCollection.find({'username': username}) is not None:
-            print("account found")
+        if userCollection.find_one({'username': username}) is not None:
+            print("account found", file=sys.stderr)
             user = userCollection.find_one({'username': username})
-            check_password = bcrypt.checkpw(password.encode(), user['password'])
+            if bcrypt.checkpw(password.encode(), user['password']):
+                print("logged in", file=sys.stderr)
         resp.set_cookie('username', username)
     return resp
 
@@ -41,9 +43,8 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
         user_entry = helper_functions.create_account(username, password)
-        if userCollection.find(user_entry['username']) is None:
-            userCollection.insert(user_entry)
-            print("you made an account")
+        userCollection.insert_one(user_entry)
+        print("you made an account", file=sys.stderr)
         #put code for front end
     return current_app.send_static_file('register.html')
 
